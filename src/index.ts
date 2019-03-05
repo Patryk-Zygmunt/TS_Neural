a31109d913340e4f03da0d58b3197dc00164cefd
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2019-03-05 13:45:21 +0100

    eeeeeeeeeeeeeeeeee

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import {Nueron} from "./neuron";
-var i:string = "iiii"
-console.log(i)
-var n :Nueron = new Nueron([],[],3);
-console.log(n)
+import {func, Nueron} from "./neuron";
+import {Matrix} from "mathjs";
+import math = require("mathjs");
+
+var weight : Matrix = math.matrix([func.random(),func.random(),func.random()]);
+var training_in:Matrix = math.matrix([[0, 0,1], [1,1,1], [1,0,1]]);
+var training_out: Matrix = math.transpose(math.matrix([[0,1,1]])) as Matrix;
+//console.log(weight)//console.log(training_in)
+//console.log (func.sig(math.multiply(training_in,weight) as number[]))
+//for (let i = 0; i <1000 ; i++) {
+    var out = func.sig(math.multiply(training_in, weight) as number[])
+console.log(out);console.log(training_out)
+    var er = math.subtract(training_out, math.transpose(out))
+console.log(er)
+    var er_sig = math.multiply(er, func.sig_d(out as number[]))
+    var adjustments = math.multiply(math.transpose(training_in), er_sig)
+    weight = math.add(weight, adjustments) as Matrix;
+//}
+
+console.log(weight)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/neuron.ts
+++ src/neuron.ts
@@ -1,13 +1,21 @@
+import math = require("mathjs");
+
+
 export class Nueron{
     weights : number[]=[];
     input: number[]=[];
-    output:number;
+    output: number[]=[];
 
-    constructor(weights: number[], input: number[], output: number) {
-        this.weights = weights;
+    constructor( input: number[], output: number[]) {
         this.input = input;
         this.output = output;
     }
+
+
+    static  init():Nueron{
+        return new Nueron([],[])
+    }
+
 
 
     toString():string{
@@ -15,4 +23,16 @@ export class Nueron{
     }
 }
 
-export var t=2;
+export class func {
+
+    static random = ():number => 2 * Math.random() - 1
+
+    static sig = (arr:number[])=>arr.map(x=>func.sigmoid(x))
+    static sig_d = (arr:number[])=>arr.map(x=>func.sigmoid_der(x))
+
+    private static  sigmoid=(x:number)=>1 / (1 + math.exp(-x))
+    private static  sigmoid_der=(x:number)=>x* (1-x)
+
+}
+
+
