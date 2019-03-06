7f9d7dad6dce2cdf22032a1e1a074361f2363f0f
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2019-03-06 12:33:27 +0100

    eeeeeeeeeeeeeeeeee

---
+++ README.md
@@ -0,0 +1,25 @@
+Training Set
+```
+ input->   out->    weight->
+ [0,0,1],  [0]      [rand(-1),1]
+ [1,1,1],  [1]      [rand(-1),1]
+ [1,0,1]   [1]      [rand(-1),1]
+ [0,1,1]   [0]
+    
+````
+Train
+ ```
+for 0 .. iteracje
+  //obliczenie wyjścia neuronu, mnożenie macierzy i wyskalowanie funkcją aktywacji
+  actual_result = sigmoid(input * weights) 
+  // obliczenie różnicy między wyjściem danych treningowych a resultatem
+  error = out - actual_result
+     /// pocho
+  error_der = error dotMultiply? sigmoid`(actual_result)
+  // obliczenie jak należy zmienić każdą z wag
+  adjustments = input.T * error_der
+  weight = weight + adjsustments  
+ ```
+ 
+ Nowa Sytuacja
+` 0 =~ sigmoid([0,1,0] * weights) `
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,21 @@ import {func, Nueron} from "./neuron";
 import {Matrix} from "mathjs";
 import math = require("mathjs");
 
-var weight : Matrix = math.matrix([func.random(),func.random(),func.random()]);
-var training_in:Matrix = math.matrix([[0, 0,1], [1,1,1], [1,0,1]]);
-var training_out: Matrix = math.transpose(math.matrix([0,1,1])) as Matrix;
-//console.log(weight)//console.log(training_in)
+var weight : Matrix = math.matrix([[func.random()],[func.random()],[func.random()]]);
+var training_in:Matrix = math.matrix([[0, 0,1], [1,1,1], [1,0,1],[0,1,1]]);
+var training_out: Matrix = math.transpose(math.matrix([[0,1,1,0]])) as Matrix;
 //console.log (func.sig(math.multiply(training_in,weight) as number[]))
-//for (let i = 0; i <1000 ; i++) {
-    var out = func.sig(math.multiply(training_in, weight) as number[])
-console.log(out);console.log(training_out)
-    var er = math.subtract(training_out, math.transpose(out))
-console.log(er)
- var d = func.sig_d(out as number[]);
-    var er_sig = math.multiply(er, func.sig_d(out as number[]))
+//for (let i = 0; i <10000 ; i++) {
+    var out = func.sig(math.multiply(training_in, weight) as number[])  //MNOŻENIE MACIERZY
+    var er:Matrix = math.subtract(training_out,out) as Matrix
+     var d = func.sig_d(out as number[]);
+    var er_sig = math.dotMultiply(er, d)
     var adjustments = math.multiply(math.transpose(training_in), er_sig)
     weight = math.add(weight, adjustments) as Matrix;
 //}
-
-console.log(weight)
+console.log(weight.toString())
+console.log(math.dot(weight,math.transpose(math.matrix([[0],[1],[0]]))
+))
 
 
 
